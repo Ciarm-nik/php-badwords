37a37027c945964f2805f8a118e17c46e86afe46
Ciarm-nik <nikcia@libero.it>
2021-05-28 15:12:59 +0200

    Inizio PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Descrizione: -->
+<!-- Creare una variabile con un paragrafo di testo. -->
+
+<?php
+
+$paragrafo = "Welcome PHP";
+
+echo $paragrafo;
+
+echo strlen($paragrafo);
+
+?>
+
+<!-- Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre * -->
+
+
